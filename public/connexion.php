60368fa2bf32724bf330913e9c08cc7e5c96fc3c
jérôme <jerome@gmail.com>
2025-06-18 15:47:36 +0000

    Modificatition

--- public/connexion.php
+++ public/connexion.php
@@ -1,11 +1,7 @@
 <?php
-$dsn = "pgsql:host=" . getenv('DB_HOST') . ";port=" . getenv('DB_PORT') . ";dbname=" . getenv('DB_NAME');
-$user = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
+$conn = pg_connect("host=localhost dbname=resultat user=jd password=jd");
 
-try {
-    $pdo = new PDO($dsn, $user, $password);
-    echo "Connexion réussie à la base de données PostgreSQL !";
-} catch (PDOException $e) {
-    echo "Erreur : " . $e->getMessage();
+if (!$conn) {
+die("Pas de connexion à postgres");
 }
+?>
